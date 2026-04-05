3a8400877539c9622f13d1bbc12ac846f6610c9e
thelaagy <laggymaya@gmail.com>
2026-04-05 00:52:31 -0300

    routes ajustment

--- src/routes.js
+++ src/routes.js
@@ -6,13 +6,15 @@ import Music from "./pages/Music.js";
 import Home from "./pages/Home.js";
 import About from "./pages/About.js";
 
+const url = "/nekoweb";
+
 export default function AppRouter() {
     return (
         <BrowserRouter>
             <Routes>
-                <Route path={"/" || "/home"} element={<Home />} />
-                <Route path="/about" element={<About />} />
-                <Route path="/music" element={<Music />} />
+                <Route path={url + "/"} element={<Home />} />
+                <Route path={url + "/about"} element={<About />} />
+                <Route path={url + "/music"} element={<Music />} />
             </Routes>
         </BrowserRouter>
     );
