5f898fcff2dd2e43080755343dadd9e6010c4177
thelaagy <laggymaya@gmail.com>
2026-04-05 23:00:15 -0300

    fix site button

--- src/pages/About.js
+++ src/pages/About.js
@@ -41,7 +41,7 @@ export default function About() {
                 >
                     <img
                         style={{ width: "auto" }}
-                        src={process.env.PUBLIC_URL + "/button.png?v=2"}
+                        src={process.env.PUBLIC_URL + "/button.png"}
                         width="88"
                         height="31"
                         alt="Laggy Website"
